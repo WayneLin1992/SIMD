5193b849f7724e898051ae3272074c97de1f111c
Wei-Han Lin <62940642+WayneLin1992@users.noreply.github.com>
2022-06-07 17:05:45 +0800

    rewrite array for neon

--- NEON.c
+++ NEON.c
@@ -1,4 +1,4 @@
-#include <immintrin.h>
+
 #include <stdio.h>
 #include <time.h>
 #include <arm_neon.h>
@@ -7,7 +7,7 @@
 int main(int argc, char* argv[]) {
     struct timespec start = {0,0}, end = {0,0};
     clock_gettime(CLOCK_MONOTONIC, &start);
-    int32x4_t first = vmovq_n_s32(40);
+    int32x4_t first = {10,20,30,40};
     int32x4_t second = vmovq_n_s32(5);
     int32x4_t result = vaddq_s32(first, second);
     clock_gettime(CLOCK_MONOTONIC, &end);
@@ -20,9 +20,9 @@ int main(int argc, char* argv[]) {
     printf("\n %.5f", ((double)end.tv_nsec + 1.0e-9*end.tv_nsec) -((double)start.tv_nsec + 1.0e-9*start.tv_nsec));
     struct timespec tstart = {0,0}, tend = {0,0};
     clock_gettime(CLOCK_MONOTONIC, &tstart);
-    int arr[8] = {40, 40, 40, 40};
-    int arr1[8] = {5, 5, 5, 5};
-    int tot[8] = {0};
+    int arr[4] = {40, 40, 40, 40};
+    int arr1[4] = {5, 5, 5, 5};
+    int tot[4] = {0};
     for(int i = 0; i < 4; i++){
         tot[i] = arr[i] + arr1[i];
     }
